140c1b0672851d9612bd95eaa1bdb56815d4f1fe
Ahmmed Sakib Noman <nomanahmmed0@gmail.com>
2022-05-03 03:32:22 +0600

    Refactored run.py

--- src/run.py
+++ src/run.py
@@ -4,24 +4,30 @@ from Word_Pos_List_Structure.Word_Pos_List_Structure import *
 from Pronoun_Finder.Pronoun_Finder import *
 from Conjunction_Finder.Conjunction_Finder import *
 
-# Taking bangla sentence as input
-Sentence = Sentence_Scanner ()
-# print ( "Sentence Scanner Object" )
-# print ( Sentence )
+class Run:
 
-# Seperate the words into a list
-word_seperator = Word_Seperator ( Sentence.sentence )
-# print ( "Word Seperator Object" )
-# print ( word_seperator )
- 
-# Creating the struct with word and pos list
-word_pos_list = Word_Pos_List_Structure ( word_seperator.words )
-# print ( word_pos_list )
+    Sentence:Sentence_Scanner
+    word_seperator:Word_Seperator
+    word_pos_list:Word_Pos_List_Structure
+    pronoun_finder:Pronoun_Finder
+    conjunction_finder:Conjunction_Finder
 
-# Pronoun finder class
-pronoun_finder = Pronoun_Finder ( word_pos_list.word_pos )
-# print ( pronoun_finder )
+    def __init__ ( self ):
+        pass
 
-# Conjunction finder class
-conjunction_finder = Conjunction_Finder ( pronoun_finder.word_pos )
-print ( conjunction_finder )
+    def start ( self ):
+        # Taking bangla sentence as input
+        Sentence = Sentence_Scanner ()
+        # Seperate the words into a list
+        word_seperator = Word_Seperator ( Sentence.sentence )
+        # Creating the struct with word and pos list
+        word_pos_list = Word_Pos_List_Structure ( word_seperator.words )
+        # Pronoun finder class
+        pronoun_finder = Pronoun_Finder ( word_pos_list.word_pos )
+        # Conjunction finder class
+        conjunction_finder = Conjunction_Finder ( pronoun_finder.word_pos )
+        print ( conjunction_finder )
+
+if __name__ == "__main__":
+    run = Run ()
+    run.start ()
